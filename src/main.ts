78f465f0c5b008b5c47d2c2c46c4aaaa375266f3
Hibikine Kage <goodsmile314159@gmail.com>
2019-09-04 12:41:16 +0900

    add calendar validate

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,42 @@
-function getCalenadar() {
-  const calendars = Calendar.CalendarList.list({maxResults: 100});
-  const calendarNames = calendars.items.map(v => v.summary)
-  console.log(calendarNames)
-  console.log(calendars.items.map(v=>v.accessRole))
-  return calendars;
+let calendarsCache:
+  | GoogleAppsScript.Calendar.Schema.CalendarListEntry[]
+  | null = null;
+function getCalendars() {
+  if (calendarsCache !== null) {
+    return calendarsCache;
+  }
+  const calendars = Calendar.CalendarList.list({ maxResults: 100 });
+  const ownerCalendars = calendars.items.filter(
+    ({ accessRole }) => accessRole === 'owner'
+  );
+  calendarsCache = ownerCalendars;
+  return ownerCalendars;
 }
+
+function getCalendarNames() {
+  const calendars = getCalendars();
+  return calendars.map(v => v.summary);
+}
+
+function addCalendarValidate() {
+  const calendarNames = getCalendarNames();
+  const sheet = SpreadsheetApp.getActiveSpreadsheet()
+    .getSheets()[0]
+    .getRange('B2:B');
+  const rule = SpreadsheetApp.newDataValidation()
+    .requireValueInList(calendarNames)
+    .build();
+  sheet.setDataValidation(rule);
+}
+
+function update() {
+  addCalendarValidate();
+}
+
+function getSchedulesTester() {
+  getSchedules;
+}
+
+function getSchedules(
+  calendar: GoogleAppsScript.Calendar.Schema.CalendarListEntry
+) {}
